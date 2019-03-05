b0cfdab81e822ed17fcfe1664cbac10acac14ee2
Dmitry <mit0787@gmail.com>
2019-03-06 00:43:43 +0300

    Сверстал блоки с ценами и шагами, добавил анимацию

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,6 +12,20 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+  var button = $("#offer-button");
+  var modal = $("#modal");
+  var close = $("#close");
+
+  button.on("click", function () {
+    modal.addClass("modal_active");
+  });
+
+  close.on("click", function () {
+    modal.removeClass("modal_active")
+  });
+});
+
 $(function() {
   $(window).scroll(function () {
     if ($(this).scrollTop() != 0) $("#top").fadeIn();
